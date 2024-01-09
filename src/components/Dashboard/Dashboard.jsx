ac462c4d05aca59512ef94458a5f031ee91c2298
Amit Mishra <coolamitgolu567@gmail.com>
2024-01-09 17:16:08 +0530

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,77 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import Navbar from "../Navbar/Navbar";
-// import Sidebar from "../Sidebar/Sidebar";
-// import { Box, Flex, useMediaQuery } from "@chakra-ui/react";
-// import DashboardContent from "./DashboardContent";
-// import { Routes, Route } from 'react-router-dom';
-// import GoalDashboard from "../Goals/GoalDashboard";
-// import SingleGoalPage from "../Goals/SingleGoalPage";
-// import ExpenseCalendar from "../Expense/ExpenseCalender";
-// import PortfolioDashboard from "../Portfolio/PortfolioDashboard";
-// import BankPortfolio from "../Portfolio/BankPortfolio";
-
-// const Dashboard = () => {
-
-//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-//   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
-
-//   const sidebarWidth = isLargerThan768 ? "15%" : "5%";
-
-//   const toggleSidebar = () => {
-//     setIsSidebarOpen(!isSidebarOpen);
-//   };
-
-//   useEffect(() => {
-//     setIsSidebarOpen(isLargerThan768);
-//   }, [isLargerThan768]);
-
-//   return (
-//     <Flex direction="column" height="100vh">
-//       <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-//       <Flex flexGrow={1} overflow="hidden">
-//         {isSidebarOpen && (
-//           <Box
-//             width={{ base: "100%", md: sidebarWidth }}
-//             transform={{
-//               base: "translateX(0)",
-//               md: isSidebarOpen ? "translateX(0)" : "translateX(-100%)",
-//             }}
-//             transition="transform 0.5s"
-//           >
-//             <Sidebar />
-//           </Box>
-//         )}
-//         <Box flexGrow={1} p="4" overflow="auto"
-//           css={{
-//             "::-webkit-scrollbar": {
-//               width: "12px",
-//             },
-//             "::-webkit-scrollbar-thumb": {
-//               backgroundColor: "rgba(0, 0, 0, 0.2)",
-//               borderRadius: "8px",
-//             },
-//             "::-webkit-scrollbar-track": {
-//               backgroundColor: "rgba(0, 0, 0, 0.1)",
-//             },
-//           }}>
-//           <Routes>
-//             <Route path="/" element={<DashboardContent />} />
-//             <Route path="/goals" element={<GoalDashboard />} />
-//             <Route path="/goal/:goalId" element={<SingleGoalPage />} />
-//             <Route path="/expenses" element={<ExpenseCalendar />} />
-//             <Route path="/portfolio" element={<PortfolioDashboard/>} />
-//             <Route path="/bank-portfolio" element={<BankPortfolio/>} />
-
-//             {/* Add more routes as needed */}
-//           </Routes>
-//         </Box>
-//       </Flex>
-//     </Flex>
-//   );
-// };
-
-// export default Dashboard;
-
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
@@ -84,8 +10,8 @@ import SingleGoalPage from "../Goals/SingleGoalPage";
 import ExpenseCalendar from "../Expense/ExpenseCalender";
 import PortfolioDashboard from "../Portfolio/PortfolioDashboard";
 import BankPortfolio from "../Portfolio/BankPortfolio";
-import ContactUs from "../ContactUs/Contactus";
 import IncompleteComponent from "../IncompleteComponent/IncompleteComponent";
+import SinglePortfolioPage from "../Portfolio/SinglePortfolioPage";
 
 const Dashboard = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -141,10 +67,11 @@ const Dashboard = () => {
             <Route path="/expenses" element={<ExpenseCalendar />} />
             <Route path="/portfolio" element={<PortfolioDashboard />} />
             <Route path="/bank-portfolio" element={<BankPortfolio />} />
-            <Route path="/contact-us" element={<ContactUs />} />
             <Route path="/dreams" element={<IncompleteComponent />} />
             <Route path="/achievement" element={<IncompleteComponent />} />
             <Route path="/comingsoon" element={<IncompleteComponent />} />
+            <Route path="/portfolio/:portfolioId" element={<SinglePortfolioPage />} />
+
 
 
             {/* Add more routes as needed */}
